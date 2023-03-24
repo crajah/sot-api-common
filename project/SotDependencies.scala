1b05754aca255a90559d0f6ed63d2ac9cfbdc40b
Chandan Rajah <chandan.rajah@gmail.com>
2023-03-24 18:56:21 +0000

    Bumped sot_common_secure

--- project/SotDependencies.scala
+++ project/SotDependencies.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 object SotDependencies {
-  val sotCommonSecure = "parallelai" %% "sot_common_secure" % "0.1.20"
+  val sotCommonSecure = "parallelai" %% "sot_common_secure" % "0.1.21"
 }
