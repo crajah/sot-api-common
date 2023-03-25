f1704e08bd10e4015c2a57da0864a1ef9c8a4679
Chandan Rajah <chandan.rajah@gmail.com>
2023-03-25 20:17:27 +0000

    Result Ops for Endpoints

--- src/main/scala/parallelai/sot/api/http/endpoints/EndpointOps.scala
+++ src/main/scala/parallelai/sot/api/http/endpoints/EndpointOps.scala
@@ -6,6 +6,7 @@ import io.finch.Output
 import com.twitter.bijection.Conversion.asMethod
 import com.twitter.bijection.twitter_util.UtilBijections._
 import com.twitter.finagle.http.Status
+import parallelai.sot.api.http.Result
 
 trait EndpointOps {
   type TFuture[A] = com.twitter.util.Future[A]
@@ -16,18 +17,31 @@ trait EndpointOps {
     def toOutput: Future[Output[Response]] = f recover {
       case ResponseException(r) => r
       case t: Throwable => Response(Error(t.getMessage), Status.InternalServerError)
-    } map { callResponse =>
-      Output.payload(callResponse, callResponse.status)
+    } map { response =>
+      Output.payload(response, response.status)
     }
 
-    def toTFuture: TFuture[Output[Response]] =
-      toOutput.as[TFuture[Output[Response]]]
+    def toTFuture: TFuture[Output[Response]] = toOutput.as[TFuture[Output[Response]]]
   }
 
   implicit class ResponseOps(r: Response) {
     import cats.implicits._
 
     def toTFuture: TFuture[Output[Response]] = r.pure[Future].toTFuture
+  }
+
+  implicit class FutureResultOps[T](f: Future[Result[T]]) {
+    def toOutput: Future[Output[Result[T]]] =
+      f.map(r => Output.payload(r, r.status))
+
+    def toTFuture: TFuture[Output[Result[T]]] =
+      toOutput.as[TFuture[Output[Result[T]]]]
+  }
+
+  implicit class ResultOps[T](r: Result[T]) {
+    import cats.implicits._
+
+    def toTFuture: TFuture[Output[Result[T]]] = r.pure[Future].toTFuture
   }
 
   implicit class PathContext(val sc: StringContext) {
