1e83cd46fc7109afc1994dd8db7bda33506486a7
Chandan Rajah <chandan.rajah@gmail.com>
2023-03-25 18:29:59 +0000

    IdGenerator99UniqueSuffix object

--- src/main/scala/parallelai/sot/api/http/endpoints/Error.scala
+++ src/main/scala/parallelai/sot/api/http/endpoints/Error.scala
@@ -4,6 +4,7 @@ import spray.json.DefaultJsonProtocol._
 import spray.json._
 
 // TODO - There is an io.finch.Error related to endpoint errors... could we use that instead?
+@deprecated(message = "A new Error maybe created but currently using the new Errors", since = "17th March 2018")
 case class Error(message: String)
 
 object Error {
--- src/test/scala/parallelai/sot/api/model/IdGenerator99UniqueSuffix.scala
+++ src/test/scala/parallelai/sot/api/model/IdGenerator99UniqueSuffix.scala
@@ -1,5 +1,7 @@
 package parallelai.sot.api.model
 
+object IdGenerator99UniqueSuffix extends IdGenerator99UniqueSuffix
+
 trait IdGenerator99UniqueSuffix extends IdGenerator {
   override protected def uniqueSuffix: String = "99"
 }
