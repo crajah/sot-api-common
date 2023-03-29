80db75eac465ef7217c74591d66b0bd6f80ab5a5
Chandan Rajah <chandan.rajah@gmail.com>
2023-03-29 10:28:41 +0000

    Upgrade common-secure

--- project/SotDependencies.scala
+++ project/SotDependencies.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 object SotDependencies {
-  val sotCommonSecure = "parallelai" %% "sot_common_secure" % "0.1.27"
+  val sotCommonSecure = "parallelai" %% "sot_common_secure" % "0.1.29"
 }
