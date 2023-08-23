30511b49f7f755d399d6591d0a6d179be5295ebf
Chandan Rajah <chandan.rajah@gmail.com>
2023-08-23 09:58:47 +0000

    revert deprecation of parallelai.sot.api.http.Response

--- src/main/scala/parallelai/sot/api/http/endpoints/Response.scala
+++ src/main/scala/parallelai/sot/api/http/endpoints/Response.scala
@@ -7,7 +7,7 @@ import spray.json.lenses.JsonLenses._
 import com.twitter.finagle.http.Status
 import parallelai.sot.api.json.SprayJsonLens._
 
-@deprecated(message = "Use parallelai.sot.api.http.Result", since = "17th March 2018")
+//@deprecated(message = "Use parallelai.sot.api.http.Result", since = "17th March 2018")
 case class Response(content: JsValue, status: Status)
 
 // TODO - Hateoas, such as links
@@ -34,14 +34,14 @@ object Response extends DefaultJsonProtocol {
     new Response(implicitly[JsonWriter[J]].write(j), status)
 
   // TODO - There is an io.finch.Error related to endpoint errors... could we use that instead?
-  @deprecated(message = "A new Error maybe created but currently using the new Errors", since = "17th March 2018")
+//  @deprecated(message = "A new Error maybe created but currently using the new Errors", since = "17th March 2018")
   case class Error(message: String)
 
   object Error {
     implicit val rootJsonFormat: RootJsonFormat[Error] = jsonFormat(Error.apply, "error-message")
   }
 
-  @deprecated(message = "Use parallelai.sot.api.http.Errors", since = "17th March 2018")
+//  @deprecated(message = "Use parallelai.sot.api.http.Errors", since = "17th March 2018")
   case class Errors(messages: String*)
 
   object Errors {
